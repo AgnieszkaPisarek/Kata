636d9c7f9d1893e649d5cf4b8f3d87db73bf4d2f
AgnieszkaPisarek <agnieszka.pisarek@klosebrothers.de>
2024-10-17 14:05:11 +0200

    Tennis kata completed

--- Tennis/src/main/java/Tennis.java
+++ Tennis/src/main/java/Tennis.java
@@ -3,7 +3,7 @@ public class Tennis {
     private int playerOneScore;
     private int playerTwoScore;
 
-    public String playTennis() {
+    public String startTheGameOfTennis() {
         playerOneScore = 0;
         playerTwoScore = 0;
         return "The game has started!";
@@ -17,12 +17,8 @@ public class Tennis {
         playerTwoScore += 1;
     }
 
-    public String isADeuce()    {
-
-        if(playerOneScore == playerTwoScore && playerOneScore > 3) {
-            return "It's a Deuce!";
-        }
-        return "Not a Deuce.";
+    public boolean isADeuce()    {
+        return playerOneScore == playerTwoScore && playerOneScore >= 3;
     }
 
     public int getPlayerOneScore() {
@@ -34,10 +30,34 @@ public class Tennis {
     }
 
     public String hasAdvantage() {
-        if(playerOneScore > playerTwoScore) {
+        if(playerOneScore > playerTwoScore && (playerOneScore + playerTwoScore) > 6) {
             return "Player one has an Advantage!";
         }
-        return null;
+
+        if(playerTwoScore > playerOneScore && (playerOneScore + playerTwoScore) > 6) {
+            return "Player two has an Advantage!";
+        }
+        return "No Advantage.";
+    }
+
+    public String whoWon()  {
+        if(playerTwoScore - playerOneScore >= 2 && playerTwoScore >= 4) {
+            return "Player two won!";
+        }
+        if(playerOneScore - playerTwoScore >= 2 && playerOneScore >= 4) {
+            return "Player one won!";
+        }
+        return "No win.";
+    }
+
+    public String getTennisPoints(int points)   {
+        return switch (points) {
+            case 0 -> "love";
+            case 1 -> "15";
+            case 2 -> "30";
+            case 3 -> "40";
+            default -> null;
+        };
     }
 
 }
--- Tennis/src/test/java/TennisTest.java
+++ Tennis/src/test/java/TennisTest.java
@@ -11,13 +11,13 @@ class TennisTest {
     @BeforeEach
     public void setUp() {
         tennis = new Tennis();
-        tennis.playTennis();
+        tennis.startTheGameOfTennis();
     }
 
     @Test
     public void shouldSetUpTheTennisGame() {
         assertAll(
-                () -> assertThat(tennis.playTennis()).isEqualTo("The game has started!"),
+                () -> assertThat(tennis.startTheGameOfTennis()).isEqualTo("The game has started!"),
                 () -> assertThat(tennis.getPlayerOneScore()).isEqualTo(0),
                 ()  -> assertThat(tennis.getPlayerTwoScore()).isEqualTo(0));
     }
@@ -49,27 +49,27 @@ class TennisTest {
     }
 
     @Test
-    public void shouldBeDeuce_whenBothPlayerHaveFourPoints() {
+    public void shouldBeDeuce_whenBothPlayerHaveThreePoints() {
 
-        for(int i = 0; i < 4; i++)  {
-            tennis.playerOneGetsAPoint();
-            tennis.playerTwoGetsAPoint();
-        }
-        assertThat(tennis.isADeuce()).isEqualTo("It's a Deuce!");
-    }
-
-    @Test
-    public void shouldNotBeADeuce_whenBothPlayerHaveLessThanFourPoints() {
         for(int i = 0; i < 3; i++)  {
             tennis.playerOneGetsAPoint();
             tennis.playerTwoGetsAPoint();
         }
-        assertThat(tennis.isADeuce()).isEqualTo("Not a Deuce.");
+        assertThat(tennis.isADeuce()).isEqualTo(true);
+    }
+
+    @Test
+    public void shouldNotBeADeuce_whenBothPlayerHaveLessThanThreePoints() {
+        for(int i = 0; i < 2; i++)  {
+            tennis.playerOneGetsAPoint();
+            tennis.playerTwoGetsAPoint();
+        }
+        assertThat(tennis.isADeuce()).isEqualTo(false);
     }
 
     @Test
     public void shouldBeAdvantage_whenOnePlayerHasMorePointsAfterDeuce() {
-        for(int i = 0; i < 4; i++)  {
+        for(int i = 0; i < 3; i++)  {
             tennis.playerOneGetsAPoint();
             tennis.playerTwoGetsAPoint();
         }
@@ -78,6 +78,89 @@ class TennisTest {
         assertThat(tennis.hasAdvantage()).isEqualTo("Player one has an Advantage!");
     }
 
+    @Test
+    public void shouldBeAdvantage_whenPlayerTwoHasMorePointsAfterDeuce() {
+        for(int i = 0; i < 3; i++)  {
+            tennis.playerOneGetsAPoint();
+            tennis.playerTwoGetsAPoint();
+        }
 
+        tennis.playerTwoGetsAPoint();
+        assertThat(tennis.hasAdvantage()).isEqualTo("Player two has an Advantage!");
+    }
+
+    @Test
+    public void shouldNotBeAdvantage_whenThereIsNoDeuce() {
+        for(int i = 0; i < 2; i++)  {
+            tennis.playerOneGetsAPoint();
+            tennis.playerTwoGetsAPoint();
+        }
+
+        tennis.playerTwoGetsAPoint();
+        assertThat(tennis.hasAdvantage()).isEqualTo("No Advantage.");
+    }
+
+    @Test
+    public void playerTwoShouldWin() {
+        for(int i = 0; i < 2; i++)  {
+            tennis.playerOneGetsAPoint();
+            tennis.playerTwoGetsAPoint();
+        }
+
+        tennis.playerTwoGetsAPoint();
+        tennis.playerTwoGetsAPoint();
+        assertThat(tennis.whoWon()).isEqualTo("Player two won!");
+    }
+
+    @Test
+    public void playerOneShouldWin() {
+        for(int i = 0; i < 2; i++)  {
+            tennis.playerOneGetsAPoint();
+            tennis.playerTwoGetsAPoint();
+        }
+
+        tennis.playerOneGetsAPoint();
+        tennis.playerOneGetsAPoint();
+        assertThat(tennis.whoWon()).isEqualTo("Player one won!");
+    }
+
+    @Test
+    public void shouldNoOneWin_whenPointsAreBelowFour() {
+        tennis.playerOneGetsAPoint();
+        tennis.playerOneGetsAPoint();
+
+        assertThat(tennis.whoWon()).isEqualTo("No win.");
+
+    }
+
+    @Test
+    public void pointsShouldEqualLove_whenThereIsNoPoints() {
+        int scoredPoints = tennis.getPlayerOneScore();
+        assertThat(tennis.getTennisPoints(scoredPoints)).isEqualTo("love");
+    }
+
+    @Test
+    public void pointsShouldEqualFifteen_whenThereIsOnePoint() {
+        tennis.playerOneGetsAPoint();
+        int scoredPoints = tennis.getPlayerOneScore();
+        assertThat(tennis.getTennisPoints(scoredPoints)).isEqualTo("15");
+    }
+
+    @Test
+    public void pointsShouldEqualThirty_whenThereAreTwoPoint() {
+        tennis.playerOneGetsAPoint();
+        tennis.playerOneGetsAPoint();
+        int scoredPoints = tennis.getPlayerOneScore();
+        assertThat(tennis.getTennisPoints(scoredPoints)).isEqualTo("30");
+    }
+
+    @Test
+    public void pointsShouldEqualForty_whenThereAreTwoPoint() {
+        tennis.playerOneGetsAPoint();
+        tennis.playerOneGetsAPoint();
+        tennis.playerOneGetsAPoint();
+        int scoredPoints = tennis.getPlayerOneScore();
+        assertThat(tennis.getTennisPoints(scoredPoints)).isEqualTo("40");
+    }
 
 }
