86dc180eca54738a2fe4fd33ac947ff5b754dada
AgnieszkaPisarek <agnieszka.pisarek@klosebrothers.de>
2024-10-17 08:50:48 +0200

    ChristmasTree done

--- ChristmasTree/src/main/java/org/example/ChristmasTree.java
+++ ChristmasTree/src/main/java/org/example/ChristmasTree.java
@@ -1,5 +1,35 @@
 package org.example;
 
+import java.util.ArrayList;
+
 public class ChristmasTree {
 
+    public ArrayList<String> draw(int height)    {
+
+        ArrayList<String> tree = new ArrayList<>();
+        StringBuilder level;
+
+        int spaces = height - 1;
+        int signs  = 1;
+
+
+        for (int i = 0; i < height; i++) {
+
+            level = new StringBuilder();
+
+            level.append(" ".repeat(spaces));
+            level.append("X".repeat(signs));
+            level.append(" ".repeat(spaces));
+
+            signs += 2;
+            spaces--;
+
+            tree.add(level.toString());
+        }
+
+        String lastLevel = tree.getFirst();
+        tree.add(lastLevel.replaceFirst("X", "|"));
+
+        return tree;
+    }
 }
--- ChristmasTree/src/test/java/org/example/ChristmasTreeTest.java
+++ ChristmasTree/src/test/java/org/example/ChristmasTreeTest.java
@@ -2,11 +2,52 @@ package org.example;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 class ChristmasTreeTest {
     @Test
-    void ThisShouldBeGreen() {
-        assertThat(true).isTrue();
+    void shouldReturnOneLevelTree_whenGivenOne() {
+
+        final var christmasTree = new ChristmasTree();
+        ArrayList<String> tree;
+        tree = christmasTree.draw(1);
+
+        assertAll(
+                () -> assertThat(tree.getFirst()).isEqualTo("X"),
+                () -> assertThat(tree.getLast()).isEqualTo("|")
+        );
     }
+
+    @Test
+    void shouldReturnTwoLevelTree_whenGivenTwo() {
+
+        final var christmasTree = new ChristmasTree();
+        ArrayList<String> tree;
+        tree = christmasTree.draw(2);
+
+        assertAll(
+                () -> assertThat(tree.getFirst()).isEqualTo(" X "),
+                () -> assertThat(tree.get(1)).isEqualTo("XXX"),
+                () -> assertThat(tree.getLast()).isEqualTo(" | ")
+        );
+    }
+
+    @Test
+    void shouldReturnThreeLevelTree_whenGivenThree() {
+
+        final var christmasTree = new ChristmasTree();
+        ArrayList<String> tree;
+        tree = christmasTree.draw(3);
+
+        assertAll(
+                () -> assertThat(tree.getFirst()).isEqualTo("  X  "),
+                () -> assertThat(tree.get(1)).isEqualTo(" XXX "),
+                () -> assertThat(tree.get(2)).isEqualTo("XXXXX"),
+                () -> assertThat(tree.getLast()).isEqualTo("  |  ")
+        );
+    }
+
 }
