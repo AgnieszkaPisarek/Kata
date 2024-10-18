c5fc55f9974518f054c1c9273147aa73558c034e
AgnieszkaPisarek <agnieszka.pisarek@klosebrothers.de>
2024-10-18 12:28:33 +0200

    Roman Numerals kata - improved

--- RomanNumerals/src/main/java/RomanNumerals.java
+++ RomanNumerals/src/main/java/RomanNumerals.java
@@ -1,71 +1,47 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class RomanNumerals {
     public String convertToRoman(int number) {
+
         StringBuilder roman = new StringBuilder();
+        List<String[]> signs = new ArrayList<>();
 
-            while (number - 1000 >= 0) {
-                roman.append("M");
-                number -= 1000;
+        signs.add(new String[]{"M", "CM", "D", "CD"});
+        signs.add(new String[]{"C", "XC", "L", "XL"});
+        signs.add(new String[]{"X", "IX", "V", "IV"});
+        signs.add(new String[]{"I", "-", "-", "-"});
+
+        int[] breakpoints = new int[]{1000, 900, 500, 400};
+
+        for (int i = 0; i < 4; i++) {
+
+            while (number >= breakpoints[0]) {
+                roman.append(signs.get(i)[0]);
+                number -= breakpoints[0];
             }
 
-            if (number > 900)    {
-                roman.append("CM");
-                number -= 900;
+            if (number == 0)    return roman.toString();
+
+            if (number >= breakpoints[1])    {
+                roman.append(signs.get(i)[1]);
+                number -= breakpoints[1];
             }
 
-            if (number >= 500)    {
-                roman.append("D");
-                number -= 500;
+            if (number >= breakpoints[2])    {
+                roman.append(signs.get(i)[2]);
+                number -= breakpoints[2];
             }
 
-            if (number > 399)    {
-                roman.append("CD");
-                number -= 400;
+            if (number >= breakpoints[3])    {
+                roman.append(signs.get(i)[3]);
+                number -= breakpoints[3];
             }
 
-            while (number - 100 >= 0) {
-                roman.append("C");
-                number -= 100;
+            for(int j = 0; j < 4; j++) {
+                breakpoints[j] /= 10;
             }
-
-            if (number > 89)    {
-                roman.append("XC");
-                number -= 90;
-            }
-
-            while (number - 50 >= 0) {
-                roman.append("L");
-                number -= 50;
-            }
-
-            if (number > 39)    {
-                roman.append("XL");
-                number -= 40;
-            }
-
-            while (number - 10 >= 0) {
-                roman.append("X");
-                number -= 10;
-            }
-
-            if (number == 9)    {
-                roman.append("IX");
-                number -= 9;
-            }
-
-            if (number >= 5)    {
-                roman.append("V");
-                number -= 5;
-            }
-
-            if (number == 4)    {
-                roman.append("IV");
-                number -= 4;
-            }
-
-            while (number > 0) {
-                roman.append("I");
-                number--;
-            }
+        }
 
         return roman.toString();
     }
