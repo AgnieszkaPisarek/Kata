d7d0355a886f7ba12566c4efc66adf962152f884
AgnieszkaPisarek <agnieszka.pisarek@klosebrothers.de>
2024-10-24 11:12:54 +0200

    Almost done, I think

--- GameOfLife/src/main/java/GameOfLife.java
+++ GameOfLife/src/main/java/GameOfLife.java
@@ -1,5 +1,68 @@
-public class GameOfLife {
-    public void gameOfLife(int[][] board) {
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
 
+public class GameOfLife {
+
+    private int WIDTH;
+    private int HEIGHT;
+    private char[][] board;
+
+    public int getWIDTH() {
+        return WIDTH;
     }
+
+    public int getHEIGHT() {
+        return HEIGHT;
+    }
+
+    public char[][] getBoard() {
+        return board;
+    }
+
+    public void readFromFile(String filePath) throws IOException {
+        var file = new File(filePath);
+        var scanner = new Scanner(file);
+        HEIGHT = scanner.nextInt();
+        WIDTH = scanner.nextInt();
+        scanner.nextLine();
+        board = new char[WIDTH][HEIGHT];
+        int lines = 0;
+        while(scanner.hasNextLine()) {
+            board[lines] = scanner.nextLine().toCharArray();
+            lines++;
+        }
+        scanner.close();
+    }
+
+   public boolean isAlive(char cell)    {
+        return cell == '*';
+   }
+
+   public int countNeighbors(int x, int y) {
+        int neighbors = 0;
+        for(int i = x - 1; i <= x + 1; i++)   {
+            for(int j = y - 1; j <= y + 1; j++)   {
+                if(i == x && j == y)  continue;
+                if(board[i][j] == '*')   neighbors++;
+            }
+        }
+        return neighbors;
+   }
+
+   public void checkLivingConditions(int x, int y)  {
+        int neighbors = countNeighbors(x, y);
+        if(neighbors < 2 || neighbors > 3) board[x][y] = '.';
+   }
+
+    public void checkIfCellCanAwaken(int x, int y)  {
+        int neighbors = countNeighbors(x, y);
+        if(neighbors == 3) board[x][y] = '*';
+    }
+
+    public boolean checkIfInTheGrid(int x, int y) {
+        return (x >= 0 && y >= 0 && x <= WIDTH - 1 && y <= HEIGHT - 1);
+    }
+
+
 }
--- GameOfLife/src/test/java/GameOfLifeTest.java
+++ GameOfLife/src/test/java/GameOfLifeTest.java
@@ -1,5 +1,125 @@
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.Test;
 
-class GameOfLifeTest {
+import java.io.IOException;
+import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class GameOfLifeTest {
+    @Test
+    public void shouldReadHeightFromTextFile() throws IOException {
+        final var GameOfLife = new GameOfLife();
+        String filePath = "/home/agnieszka/IdeaProjects/Kata/GameOfLife/src/resources/board1.txt";
+        GameOfLife.readFromFile(filePath);
+        assertThat(GameOfLife.getHEIGHT()).isEqualTo(4);
+    }
+
+    @Test
+    public void shouldReadWidthFromTextFile() throws IOException {
+        final var GameOfLife = new GameOfLife();
+        String filePath = "/home/agnieszka/IdeaProjects/Kata/GameOfLife/src/resources/board1.txt";
+        GameOfLife.readFromFile(filePath);
+        assertThat(GameOfLife.getWIDTH()).isEqualTo(8);
+    }
+
+    @Test
+    public void shouldReadLineFromTextFile() throws IOException {
+        final var GameOfLife = new GameOfLife();
+        String filePath = "/home/agnieszka/IdeaProjects/Kata/GameOfLife/src/resources/board1.txt";
+        GameOfLife.readFromFile(filePath);
+        char[][] board = GameOfLife.getBoard();
+        assertThat(Arrays.equals(board[0], new char[]{'.', '.', '.', '.', '.', '.', '.', '.'})).isTrue();
+    }
+
+    @Test
+    public void shouldReadBoardFromTextFile() throws IOException {
+        final var GameOfLife = new GameOfLife();
+        String filePath = "/home/agnieszka/IdeaProjects/Kata/GameOfLife/src/resources/board1.txt";
+        GameOfLife.readFromFile(filePath);
+        char[][] board = GameOfLife.getBoard();
+        assertThat(Arrays.equals(board[0], new char[]{'.', '.', '.', '.', '.', '.', '.', '.'})).isTrue();
+        assertThat(Arrays.equals(board[1], new char[]{'.', '.', '.', '.', '*', '.', '.', '.'})).isTrue();
+        assertThat(Arrays.equals(board[2], new char[]{'.', '.', '.', '*', '*', '.', '.', '.'})).isTrue();
+        assertThat(Arrays.equals(board[3], new char[]{'.', '.', '.', '.', '.', '.', '.', '.'})).isTrue();
+    }
+
+    @Test
+    public void isACellAlive()  {
+        final var GameOfLife = new GameOfLife();
+        assertThat(GameOfLife.isAlive('*')).isTrue();
+    }
+
+    @Test
+    public void isACellDead()  {
+        final var GameOfLife = new GameOfLife();
+        assertThat(GameOfLife.isAlive('.')).isFalse();
+    }
+
+   @Test
+    public void shouldCountNeighboursForOneCell() throws IOException {
+       final var GameOfLife = new GameOfLife();
+       String filePath = "/home/agnieszka/IdeaProjects/Kata/GameOfLife/src/resources/board1.txt";
+       GameOfLife.readFromFile(filePath);
+       char[][] board = GameOfLife.getBoard();
+       assertThat(Arrays.equals(board[0], new char[]{'.', '.', '.', '.', '.', '.', '.', '.'})).isTrue();
+       assertThat(Arrays.equals(board[1], new char[]{'.', '.', '.', '.', '*', '.', '.', '.'})).isTrue();
+       assertThat(Arrays.equals(board[2], new char[]{'.', '.', '.', '*', '*', '.', '.', '.'})).isTrue();
+       assertThat(Arrays.equals(board[3], new char[]{'.', '.', '.', '.', '.', '.', '.', '.'})).isTrue();
+
+       assertThat(GameOfLife.countNeighbors(1,4)).isEqualTo(2);
+   }
+
+    @Test
+    public void underpopulatedCellDies_whenThereAreLessThanTwoCellsAround() throws IOException {
+        final var GameOfLife = new GameOfLife();
+        String filePath = "/home/agnieszka/IdeaProjects/Kata/GameOfLife/src/resources/underpopulatedCells.txt";
+        GameOfLife.readFromFile(filePath);
+        GameOfLife.checkLivingConditions(2, 3);
+        char[][] board = GameOfLife.getBoard();
+        assertThat(board[2][3]).isEqualTo('.');
+    }
+
+    @Test
+    public void overcrowdedCellDies_whenThereAreMoreThanThreeLivingCellsAround() throws IOException {
+        final var GameOfLife = new GameOfLife();
+        String filePath = "/home/agnieszka/IdeaProjects/Kata/GameOfLife/src/resources/overcrowdedCells.txt";
+        GameOfLife.readFromFile(filePath);
+        GameOfLife.checkLivingConditions(2, 3);
+        char[][] board = GameOfLife.getBoard();
+        assertThat(board[2][3]).isEqualTo('.');
+    }
+
+    @Test
+    public void cellShouldAwaken_whenThereAreThreeLiveNeighbours() throws IOException {
+        final var GameOfLife = new GameOfLife();
+        String filePath = "/home/agnieszka/IdeaProjects/Kata/GameOfLife/src/resources/canAwakenCell.txt";
+        GameOfLife.readFromFile(filePath);
+        GameOfLife.checkIfCellCanAwaken(2, 3);
+        char[][] board = GameOfLife.getBoard();
+        assertThat(board[2][3]).isEqualTo('*');
+    }
+
+    @Test
+    public void checkIfXIsInTheGrid() throws IOException {
+        final var GameOfLife = new GameOfLife();
+        String filePath = "/home/agnieszka/IdeaProjects/Kata/GameOfLife/src/resources/canAwakenCell.txt";
+        GameOfLife.readFromFile(filePath);
+        assertThat(GameOfLife.checkIfInTheGrid(-1, 0)).isEqualTo(false);
+        assertThat(GameOfLife.checkIfInTheGrid(10, 0)).isEqualTo(false);
+        assertThat(GameOfLife.checkIfInTheGrid(2, 2)).isEqualTo(true);
+    }
+
+    @Test
+    public void checkIfYIsInTheGrid() throws IOException {
+        final var GameOfLife = new GameOfLife();
+        String filePath = "/home/agnieszka/IdeaProjects/Kata/GameOfLife/src/resources/canAwakenCell.txt";
+        GameOfLife.readFromFile(filePath);
+        assertThat(GameOfLife.checkIfInTheGrid(2, -1)).isEqualTo(false);
+        assertThat(GameOfLife.checkIfInTheGrid(2, 8)).isEqualTo(false);
+        assertThat(GameOfLife.checkIfInTheGrid(5, 3)).isEqualTo(true);
+    }
+
+
+
 
 }
