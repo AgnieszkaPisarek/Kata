1d4aef8f8c7c8aa56d75555649e19566221fb158
agnieszka <agnieszka.pisarek@klosebrothers.com>
2024-11-04 08:01:10 +0100

    ChristmasTree refactored

--- ChristmasTree/src/main/java/org/example/ChristmasTree.java
+++ ChristmasTree/src/main/java/org/example/ChristmasTree.java
@@ -1,35 +1,48 @@
 package org.example;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class ChristmasTree {
+    public static final int NUMBER_OF_NEW_BRANCHES_PER_LEVEL = 2;
+    public static String SPACES = " ";
+    public static String ROOT = "|";
+    public static String BRANCH = "X";
 
-    public ArrayList<String> draw(int height)    {
+    public List<String> makeATree(int height)    {
 
-        ArrayList<String> tree = new ArrayList<>();
+        List<String> tree = new ArrayList<>();
         StringBuilder level;
 
-        int spaces = height - 1;
-        int signs  = 1;
-
+        int numberOfSpacesPerLevel = height - 1;
+        int numberOfBranchesPerLevel  = 1;
 
         for (int i = 0; i < height; i++) {
 
             level = new StringBuilder();
 
-            level.append(" ".repeat(spaces));
-            level.append("X".repeat(signs));
-            level.append(" ".repeat(spaces));
+            level.append(draw(SPACES, numberOfSpacesPerLevel));
+            level.append(draw(BRANCH, numberOfBranchesPerLevel));
+            level.append(draw(SPACES, numberOfSpacesPerLevel));
 
-            signs += 2;
-            spaces--;
+            numberOfBranchesPerLevel += NUMBER_OF_NEW_BRANCHES_PER_LEVEL;
+            numberOfSpacesPerLevel--;
 
             tree.add(level.toString());
         }
 
-        String lastLevel = tree.getFirst();
-        tree.add(lastLevel.replaceFirst("X", "|"));
+        addRoot(tree);
 
         return tree;
     }
+
+    private static void addRoot(List<String> tree) {
+        String lastLevel = tree.getFirst();
+        tree.add(lastLevel.replaceFirst(BRANCH, ROOT));
+    }
+
+    StringBuilder draw(String sign, int numberOfSignsPerLevel)   {
+        return new StringBuilder().append(sign.repeat(numberOfSignsPerLevel));
+    }
+
 }
--- ChristmasTree/src/test/java/org/example/ChristmasTreeTest.java
+++ ChristmasTree/src/test/java/org/example/ChristmasTreeTest.java
@@ -3,6 +3,7 @@ package org.example;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
@@ -11,9 +12,9 @@ class ChristmasTreeTest {
     @Test
     void shouldReturnOneLevelTree_whenGivenOne() {
 
-        final var christmasTree = new RomanNumeral();
-        ArrayList<String> tree;
-        tree = christmasTree.draw(1);
+        final var christmasTree = new ChristmasTree();
+        List<String> tree;
+        tree = christmasTree.makeATree(1);
 
         assertAll(
                 () -> assertThat(tree.getFirst()).isEqualTo("X"),
@@ -24,9 +25,9 @@ class ChristmasTreeTest {
     @Test
     void shouldReturnTwoLevelTree_whenGivenTwo() {
 
-        final var christmasTree = new RomanNumeral();
-        ArrayList<String> tree;
-        tree = christmasTree.draw(2);
+        final var christmasTree = new ChristmasTree();
+        List<String> tree;
+        tree = christmasTree.makeATree(2);
 
         assertAll(
                 () -> assertThat(tree.getFirst()).isEqualTo(" X "),
@@ -38,9 +39,9 @@ class ChristmasTreeTest {
     @Test
     void shouldReturnThreeLevelTree_whenGivenThree() {
 
-        final var christmasTree = new RomanNumeral();
-        ArrayList<String> tree;
-        tree = christmasTree.draw(3);
+        final var christmasTree = new ChristmasTree();
+        List<String> tree;
+        tree = christmasTree.makeATree(3);
 
         assertAll(
                 () -> assertThat(tree.getFirst()).isEqualTo("  X  "),
